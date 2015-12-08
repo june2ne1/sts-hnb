f5cfcfe92586630e15f4436496cf3646281a3f00
HB <HB@HB-PC>
2015-12-08 17:22:59 +0900

    ddd

--- hnb/src/main/java/com/hnb/member/MemberController.java
+++ hnb/src/main/java/com/hnb/member/MemberController.java
@@ -2,9 +2,17 @@ package com.hnb.member;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
+@RequestMapping("/member")
 public class MemberController {	
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);	
-
+	
+	@RequestMapping("/join_member")
+	public String joinMember(MemberVO member){
+		return "member/join_member";
+	}
 }
--- hnb/src/main/java/com/hnb/movie/MovieController.java
+++ hnb/src/main/java/com/hnb/movie/MovieController.java
@@ -1,10 +1,15 @@
 package com.hnb.movie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hnb.admin.AdminController;
 
@@ -14,39 +19,70 @@ public class MovieController {
 	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
 	@Autowired
 	MovieServiceImpl service;
+	@Autowired
+	MovieVO movie;
 	@RequestMapping("/Movie")
 	public String movie(){
 		logger.info("MovieController-movie() 진입");
 		return "movie/Movie";
 	}
 	@RequestMapping("/movie_info")
-	public String movieInfo(){
+	public String movieInfo(Model model){
 		logger.info("MovieController-movieInfo() 진입");
+		List<MovieVO> list = service.getList();
+		model.addAttribute("movieList", list );
+		logger.info("영화리스트 조회결과 : {}", list);
 		return "movie/movie_info";
 	}
 	@RequestMapping("/movie_name")
-	public String movieName(){
+	public String movieName(String filmNumber,Model model){
 		logger.info("MovieController-movieName() 진입");
+		logger.info("영화아이디 : {}",filmNumber);
+		movie = service.searchByName(filmNumber);
+		logger.info("영화제목 : {}",movie.getFilmName());
+		model.addAttribute("movie", movie);
 		return "movie/movie_name";
 	}
 	@RequestMapping("/movie_Cut")
-	public String movieCut(){
+	public String movieCut(String filmNumber,Model model){
 		logger.info("MovieController-movieCut() 진입");
+		logger.info("영화cut : {}",filmNumber);
+		movie = service.searchByName(filmNumber);
+		String cut = movie.getCut();
+		String[]arr = cut.split("/");
+		logger.info("arr {}", arr);
+		model.addAttribute("arr", arr);
 		return "movie/movie_Cut";
 	}
 	@RequestMapping("/movie_Tra")
-	public String movieTra(){
+	public String movieTra(String filmNumber,Model model){
 		logger.info("MovieController-movieTra() 진입");
+		logger.info("영화 tra : {}",filmNumber);
+		movie = service.searchByName(filmNumber);
+		String tra = movie.getTrailer();
+		logger.info("getTrailer : {}",tra);
+		String[]arrt = tra.split("/");
+		logger.info("arrt : {}",arrt);
+		model.addAttribute("arrt", arrt);
 		return "movie/movie_Tra";
 	}
 	@RequestMapping("/movie_Basic")
-	public String movieBasic(){
+	public String movieBasic(String filmNumber,Model model){
 		logger.info("MovieController-movieBasic() 진입");
+		logger.info("영화 Basic : {}",filmNumber);
+		movie = service.searchByName(filmNumber);
+		logger.info("movieBasic컷의영화제목 : {}",movie.getFilmName());
+		model.addAttribute("movie", movie);
 		return "movie/movie_Basic";
 	}
 	@RequestMapping("/movie_Chart")
-	public String movieChart(){
+	public String movieChart(String filmNumber,Model model){
 		logger.info("MovieController-movieChart() 진입");
+		List<MovieVO> list = service.getList();
+		list = service.getList();
+		logger.info("영화차트 조회결과 : {}", list);
+		model.addAttribute("movieList2", list );
+		
 		return "movie/movie_Chart";
 	}
 	
