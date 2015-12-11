7e39de068020e0768c6ed4b5a8bb4703c95c121f
HB <HB@HB-PC>
2015-12-11 17:31:30 +0900

    DDDD

--- hnb/src/main/java/com/hnb/mapper/MovieMapper.java
+++ hnb/src/main/java/com/hnb/mapper/MovieMapper.java
@@ -2,10 +2,12 @@ package com.hnb.mapper;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.hnb.movie.MovieVO;
 
 
-
+@Repository
 public interface MovieMapper {
 	public List<MovieVO> selectAll();
 	public List<MovieVO> selectChart();
--- hnb/src/main/java/com/hnb/movie/MovieServiceImpl.java
+++ hnb/src/main/java/com/hnb/movie/MovieServiceImpl.java
@@ -2,39 +2,53 @@ package com.hnb.movie;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hnb.mapper.MovieMapper;
 
 
 
 
 @Service
 public class MovieServiceImpl implements MovieService{
-	@Autowired
-	MovieDAOImpl dao;
+	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
+	@Autowired private SqlSession sqlSession;
+	
 	@Override	//영화등록
 	public int register(MovieVO o) {
-		return dao.insert(o);
+		MovieMapper mapper = sqlSession.getMapper(MovieMapper.class);
+		return mapper.insert(o);
 	}
 	@Override	//영화수정
 	public int change(MovieVO o) {
-		return dao.update(o);
+		MovieMapper mapper = sqlSession.getMapper(MovieMapper.class);
+		return mapper.update(o);
 	}
 	@Override	//영화삭제
 	public int remove(String filmNumber) {
-		return dao.delete(filmNumber);
+		MovieMapper mapper = sqlSession.getMapper(MovieMapper.class);
+		return mapper.delete(filmNumber);
 	}
 	@Override	//영화제목으로 검색
 	public MovieVO searchByName(String filmName) {
-		return dao.selectNameBy(filmName);
+		MovieMapper mapper = sqlSession.getMapper(MovieMapper.class);
+		return mapper.selectNameBy(filmName);
 	}
 	@Override	//영화전체목록
 	public List<MovieVO> getList() {
-		return dao.selectAll();
+		logger.info("MovieServiceImpl : getList() ");
+		MovieMapper mapper = sqlSession.getMapper(MovieMapper.class);
+		List<MovieVO> list = mapper.selectAll();
+		logger.info("마이바티스 리턴값 : {}",list);
+		return list;
 	}
 	@Override
 	public List<MovieVO> getFilmNum() {
-		return dao.selectChart();
+		MovieMapper mapper = sqlSession.getMapper(MovieMapper.class);
+		return mapper.selectChart();
 	}
-
 }
